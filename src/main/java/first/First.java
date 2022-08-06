079b92bf778f17f2310d07bea9774e5e959b110f
elevoshkin <elevoshkin@rvision.ru>
2022-08-06 14:39:37 +0300

    first commit

---
+++ src/main/java/first/First.java
@@ -0,0 +1,12 @@
+package first;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class First {
+    @Test
+    void firstTest(){
+        assertTrue(3>2);
+    }
+}
+
