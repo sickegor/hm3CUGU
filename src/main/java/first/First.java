e66245b7ce05ea1e04ca84cbef2ec7ff0f0b66f4
elevoshkin <elevoshkin@rvision.ru>
2022-08-06 15:18:45 +0300

    новое что то из мастера

--- src/main/java/first/First.java
+++ src/main/java/first/First.java
@@ -6,12 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class First {
     @Test
     void firstTest(){
-        assertTrue(3>2);
+        assertTrue(3>=2);
     }
 
     @Test
     void firstTest2(){
-        assertTrue(3>2);
+        assertTrue(3>=2);
     }
 }
 
